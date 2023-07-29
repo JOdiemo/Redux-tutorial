d6d0d5404c714cee265da9ead4f555dc2b96a5ef
JOdiemo <odiemo@gmail.com>
2023-07-29 15:08:55 +0300

    following redux tutorial

---
+++ src/components/CartContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CartItem from './CartItem';
+import { clearCart } from '../cart/cartSlice';
+
+const CartContainer = () => {
+  const dispatch = useDispatch();
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => <CartItem key={item.id} {...item} />)}
+        ;
+      </div>
+      <footer>
+        <div className="cart-total">
+          <hr />
+          <h4>
+            <span>
+              $
+              {total}
+            </span>
+          </h4>
+        </div>
+        <button type="button" className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
+      </footer>
+    </section>
+  );
+};
+
+export default CartContainer;
